9b8aee4d8f4aa43ea217dfabf3d003b92312bc79
CBonnell <corey.j.bonnell@gmail.com>
2013-05-04 17:57:41 -0400

    Add name to copyright header

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/** 
- * Copyright 2013 Pavel Puchkarev
+ * Copyright 2013 Pavel Puchkarev, Corey Bonnell
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
